191599f4108045276176d09f7f726d87781031f3
Pesy Wu <admin@bili33.top>
2021-05-09 17:12:37 +0800

    Update Unlock-Theme.py

--- Unlock-Theme.py
+++ Unlock-Theme.py
@@ -1,8 +1,8 @@
-import winreg,win32api,win32con
+import winreg
 try:
     invaxionReg=winreg.OpenKey(winreg.HKEY_CURRENT_USER,r"SOFTWARE\Aquatrax\INVAXION",0,winreg.KEY_ALL_ACCESS)
 except FileNotFoundError:
-    win32api.MessageBox(0, "No invaxion data founded! Does invaxion be installed on this computer?\n未找到音灵的游戏数据！ 此电脑上安装了音灵吗？", "Warning",win32con.MB_ICONWARNING)
+    print("No invaxion data founded! Does invaxion be installed on this computer?\n未找到音灵的游戏数据！ 此电脑上安装了音灵吗？")
     exit()
 try:
     i=0
@@ -13,8 +13,8 @@ try:
             break
         i+=1
 except WindowsError:
-    win32api.MessageBox(0, "No invaxion data founded! Have you played invaxion online before April 7th, 2021?\nYou can download a save file by visiting https://share4nothing.ml/Share/\n未找到音灵的游戏数据！ 是否在停服前进行过在线游戏？\n你可以通过访问https://share4nothing.ml/Share/来获取一份存档！", "Warning",win32con.MB_ICONWARNING)
+    print("No invaxion data founded! Have you played invaxion online before April 7th, 2021?\nYou can download a save file by visiting https://share4nothing.ml/Share/\n未找到音灵的游戏数据！ 是否在停服前进行过在线游戏？\n你可以通过访问https://share4nothing.ml/Share/来获取一份存档！")
     exit()
 winreg.SetValueEx(invaxionReg,winreg.EnumValue(invaxionReg,index)[0],0,winreg.REG_BINARY,b'[{"themeId":1},{"themeId":2},{"themeId":3},{"themeId":4},{"themeId":5},{"themeId":6},{"themeId":7},{"themeId":8},{"themeId":9},{"themeId":10},{"themeId":11},{"themeId":12},{"themeId":13},{"themeId":14}]\x00')
 winreg.CloseKey(invaxionReg)
-win32api.MessageBox(0, "Unlocked/解锁成功", "Notification",win32con.MB_OK)
+print("Unlocked/解锁成功")
